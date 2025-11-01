a3c4acea99cae4b6ad4e921e6fd5870af617ad38
[dyad] <git@dyad.sh>
2025-11-01 04:15:48 -0300

    [dyad] Fixando a sidebar durante a rolagem da página - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,11 +4,11 @@ import { Header } from "./Header";
 
 export const Layout = () => {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid h-screen w-full overflow-hidden md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <Sidebar />
       <div className="flex flex-col">
         <Header />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background overflow-auto">
           <Outlet />
         </main>
       </div>
